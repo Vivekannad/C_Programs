d62729bfbc86f832e2a995b20e176bc2309eaa06
Vivek Anand <dhevikchanintha@gmail.com>
2025-01-04 20:20:22 +0500

    more functions made

--- Sales_Management/sales.c
+++ Sales_Management/sales.c
@@ -1,54 +1,83 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#define MAX_STOCK 50
 
-typedef struct Product {
+typedef struct Product
+{
     char name[100];
     int code;
     float price;
-    int quantity;
+    int stockQuantity;
     float salesValue;
 } Product;
 
-//Stock cannot be greater than 50.
+// Stock cannot be greater than 50.
 
 void clearBuffer();
-void menu();
-void inputSalesData(Product* , int);
+void inputSalesData(Product *, int);
+void calculateSalesValue(Product*, int);
+// void adjustStock(Product*,int);
+void displayProducts(Product*,int);
 
-void main(){
-int productCount = 0;
-Product products[25];
- menu();
- inputSalesData(&products[productCount], productCount);
-
+int main()
+{
+    int n;
+    printf("Enter the number of products ");
+    scanf("%d", &n);
+    clearBuffer();
+    Product products[n];
+    inputSalesData(&products[n], n);
+    calculateSalesValue(&products[n],n);
+    // adjustStock(&products[n]);
+    displayProducts(&products[n],n);
+    return 0;
 }
 
-void clearBuffer(){
-    while(getchar() != '\n');
-}
-void menu(){
-    printf("\nPress 1 ")
+void clearBuffer()
+{
+    while (getchar() != '\n');
 }
 
-void inputSalesData(Product *product, int count){
-    char c;
-    printf("Enter the name of the product ");
-    fgets(product->name, sizeof(product->name), stdin); // also adds \n if length less than given size
-    product->name[strcspn(product->name, "\n")] = '\0'; // removes extra \n
-    printf("Enter the code of the product ");
-    scanf("%d", &product->code);
-    // scanf("%d", &product->code);
-    // c = getchar();
-    // printf("\n The character returned is %c and its ASCII code is %d", c , c);
-    clearBuffer(); 
-    printf("Enter the price of the product ");
-    scanf("%f", &product->price);
-    clearBuffer();
-    printf("Enter the quantity in stock for this product ");
-    scanf("%d", &product->quantity);
-    clearBuffer();
-    printf("Enter the total Sales Value for this product ");
-    scanf("%f", &product->salesValue);
-    clearBuffer();
+void inputSalesData(Product *product, int count)
+{
+    int i;
+    for (i = 0; i < count; i++)
+    {
+        printf("Enter the name of the product ");
+        fgets(product->name, sizeof(product->name), stdin); // also adds \n if length less than given size
+        product->name[strcspn(product->name, "\n")] = '\0'; // removes extra \n
+        printf("Enter the code of the product ");
+        scanf("%d", &product->code);
+        clearBuffer();
+        printf("Enter the price of the product ");
+        scanf("%f", &product->price);
+        clearBuffer();
+        printf("Enter the quantity in stock for this product ");
+        scanf("%d", &product->stockQuantity);
+        clearBuffer();
+    }
+}
+
+void calculateSalesValue(Product * product, int n){
+    int i;
+    for(i = 0; i < n; i++){
+        (product)[i].salesValue = product[i].price * product[i].stockQuantity;
+    }
+}
+// void adjustStock(Product * product, int n){
+//     int i;
+//     for(i = 0; i < n; i++){
+//         if(product->stockQuantity > MAX_STOCK ){
+//             adjustStock(&product, n);
+
+//         }
+//     }
+// }
+
+void displayProducts(Product * product, int n){
+    int i;
+    for(i = 0; i < n; i++){
+        printf("The sales value  is %f ", product[i].salesValue);
+    }
 }
