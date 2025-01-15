c07f38cc19c710167d219fd1d4c6e5e0374dc777
Vivek Anand <dhevikchanintha@gmail.com>
2025-01-15 20:59:51 +0500

    forget password and history feature implemented

--- User_Management/user.c
+++ User_Management/user.c
@@ -4,63 +4,77 @@
 #include <conio.h>
 #include <ctype.h>
 #include <time.h>
+#include <stdbool.h>
 
 #define MAX_USERS 10
 #define CREDENTIALS 30
 
 typedef struct
 {
-    char username[30];
+    char username[CREDENTIALS];
     char password[CREDENTIALS];
+    char logInTime[20];
 } User;
 
 //Global variables
 User user[MAX_USERS];
 int userCount = 0;
 
-void clearBuffer();
-void menu();
-void userChoice(int *);
-void registerUser();
-int userLogin();
-void fix_fgets_input(char *);
-void inputCredentials(char *, char *);
-void maskPassword(char *);
-void getLogInTime();
+void clearBuffer();     //clearing the extra storage in input buffer
+void menu();        //printing the menu
+void userChoice(int *);     //getting user choice
+bool isUserAlreadyRegistered(char *);
+void registerUser();        //registering new user
+int userLogin();            // logging in of user
+void fix_fgets_input(char *);       // removing extra line By fgets
+bool inputCredentials(char *, char *);    // getting email and password with validation
+void maskPassword(char *);       //masking the password
+const char* getRegistrationTime();         //getting registration time of users
+void displayRegisteredUsersHistory();        // displaying registration time of users
+int forgetPassword();           // filtering through the array to find the user
 
 int main()
 {
     int choice;
-    // while (1)
-    // {
-    //     menu();
-    //     userChoice(&choice);
-    //     switch (choice)
-    //     {
-    //     case 1:
-    //         registerUser(&choice);
-    //         break;
-    //     case 2:
-    //         int user_index = userLogin();
-    //         if (user_index >= 0)
-    //         {
-    //             printf("Welcome back! %s ", user[user_index].username); 
-    //         }
-    //         else
-    //         {
-    //             printf("\nLogin Failed! Incorrect username or password. \n");
-    //         }
-    //         break;
-    //     case 3:
-    //         printf("\nExiting program");
-    //         exit(0);
-    //         break;
-    //     default:
-    //         printf("Enter between the choices ");
-    //     }
-    // }
-
-        getLogInTime();
+    while (1)
+    {
+        menu();
+        userChoice(&choice);
+        switch (choice)
+        {
+        case 1:
+            registerUser(&choice);  
+            break;
+        case 2:
+            int user_index = userLogin();
+            if (user_index >= 0)
+            {
+                printf("Welcome back! %s ", user[user_index].username); 
+            }
+            else
+            {
+                printf("\nLogin Failed! Incorrect username or password. \n");
+            }
+            break;
+        case 3:
+                displayRegisteredUsersHistory();
+                break;
+        case 4:
+                int index = forgetPassword();
+                if(index >= 0){
+                    printf("The password is \"%s\"", user[index].password);
+                }else{
+                    printf("User Not Found!");
+                }
+                break;
+        case 5:
+            printf("\nExiting program");
+            exit(0);
+            break;
+        default:
+            printf("Invalid Option!! ");
+        }
+    }
 
         return 0;
 }
@@ -76,11 +90,13 @@ void menu()
     printf("\n\nUser Management System");
     printf("\n1. Register");
     printf("\n2. Login");
-    printf("\n3. Exit");
+    printf("\n3. History");
+    printf("\n4. Forget Password");
+    printf("\n5. Exit");
 }
 
-void getLogInTime(){
-    char timeString[30];
+const char* getRegistrationTime(){
+    static char timeString[30]; // Use static to persist after function returns
     //time_t: A type used to store the time in seconds since the Unix epoch (January 1, 1970)
     time_t currentTime;            // Variable to store time in seconds since epoch
     // A structure that holds detailed components of time (e.g., hours, minutes, seconds, year, month, day).
@@ -90,29 +106,30 @@ void getLogInTime(){
     currentTime = time(NULL);   // seconds passed since 1st Jan 1970.
     if (currentTime == -1) {
         printf("Error getting the current time.\n");
-        return ;
+        return "";
     }
 
     // Convert to local time
     localTime = localtime(&currentTime);
     if (localTime == NULL) {
         printf("Error converting to local time.\n");
-        return ;
+        return "";
     }
 
-
-    // Display the time in a readable format
-    sprintf(timeString , "%02d:%02d:%02d" , localTime->tm_hour,
+    // Storing the time int variables in one string variable.
+    sprintf( timeString , "%02d:%02d:%02d" ,
+           localTime->tm_hour,
            localTime->tm_min,
-           localTime->tm_sec );
-        printf("The time is %s", timeString);
+           localTime->tm_sec
+            );
+    return timeString;
 }
 
 
 void userChoice(int *choice)
 {
-    printf("\nEnter your choice ");
-    scanf("%d", choice);
+        printf("\nEnter your choice ");
+   scanf("%d", choice);
     clearBuffer();
 }
 
@@ -124,10 +141,15 @@ void registerUser()
         return;
     }
     printf("Enter new User \n");
-    inputCredentials(user[userCount].username, user[userCount].password);
+    if(!inputCredentials(user[userCount].username, user[userCount].password)){
+        printf("User Already Exists!! ");
+        return;
+    }
+    //copying login time to the logInTime member of the structure.
+    strcpy(user[userCount].logInTime, getRegistrationTime());
 
-    userCount++;
     printf("\nRegistration Successful ");
+    userCount++;
 }
 
 int userLogin()
@@ -135,10 +157,12 @@ int userLogin()
     int i;
     char username[CREDENTIALS];
     char password[CREDENTIALS];
-    inputCredentials(username, password);
+    if(inputCredentials(username, password)){
+        return -1;
+    }
     for (i = 0; i < userCount; i++)
     {
-        if (strcmp(username, user[i].username) == 0 && strcmp(password, user[i].password) == 0)
+        if (strcmp(password, user[i].password) == 0)
         {
             return i;
         }
@@ -153,7 +177,7 @@ void fix_fgets_input(char *string)
     string[index] = '\0';
 }
 
-void inputCredentials(char *username, char *password){
+bool inputCredentials(char *username, char *password){
     int wantMasked = 0, isValid;
     printf("Enter the name ");
     fgets(username, CREDENTIALS, stdin);
@@ -175,6 +199,10 @@ void inputCredentials(char *username, char *password){
         fgets(password, CREDENTIALS, stdin);
         fix_fgets_input(password);
     }
+    if(!isUserAlreadyRegistered(username)){
+        return 0;
+    }
+    return 1;
 }
 
 void maskPassword(char *password){
@@ -207,6 +235,34 @@ void maskPassword(char *password){
         }
     }
     password[i] = '\0'; // null character to end the string when i index is end of string
+}
 
-    printf("\n the password is %s", password);
+void displayRegisteredUsersHistory(){
+    printf("\n%40s %40s", "User", "Time");
+    printf("\n-------------------------------------------------------------------------------------------------------------");
+    for(int i = 0; i < userCount; i++){
+        printf("\n%40s %40s", user[i].username , user[i].logInTime);
+    }
+}
+
+int forgetPassword(){
+    char findUser[CREDENTIALS];
+    printf("Enter the username :- ");
+    fgets(findUser , sizeof(findUser), stdin);
+    fix_fgets_input(findUser);
+    for(int i = 0; i < userCount; i++){
+        if(strcmp(findUser , user[i].username) == 0){
+            return i;
+        }
+    }
+    return -1;
+}
+
+bool isUserAlreadyRegistered(char *username){
+    for(int i = 0; i < userCount; i++){
+        if(strcmp(username , user[i].username) == 0){
+            return 0;
+        }
+    }
+    return 1;
 }
