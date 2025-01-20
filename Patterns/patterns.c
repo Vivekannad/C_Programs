dff821f9ae032ada012e84cce177caf365a78490
Vivek Anand <dhevikchanintha@gmail.com>
2025-01-20 16:41:06 +0500

    pattern 4 added

--- Patterns/patterns.c
+++ Patterns/patterns.c
@@ -4,11 +4,13 @@
 void pattern1();
 void pattern2();
 void pattern3();
+void pattern4();
 
 int main () {
     // pattern1();
     // pattern2();
-    pattern3();
+    // pattern3();
+    pattern4();
     return 0;
 }
 
@@ -37,6 +39,16 @@ void pattern3 () {
     for(int i = 0; i < n; i++){
         for(int j = n-i; j >= 1; j--){
             printf("*");
+        }
+        printf("\n");
+    }
+}
+
+void pattern4 () {
+    int n = 5;
+    for(int i = 1 ; i <= n; i++){
+        for(int j = 1; j <= i; j++ ) {
+            printf("%d ", j);
         }
         printf("\n");
     }
