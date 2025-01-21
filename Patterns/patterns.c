0c3849e2fc8a01d4d1a4444563eb6754f6d3a86d
Vivek Anand <dhevikchanintha@gmail.com>
2025-01-21 10:27:02 +0500

    pattern no 5 added

--- Patterns/patterns.c
+++ Patterns/patterns.c
@@ -5,12 +5,14 @@ void pattern1();
 void pattern2();
 void pattern3();
 void pattern4();
+void pattern5();
 
 int main () {
     // pattern1();
     // pattern2();
     // pattern3();
-    pattern4();
+    // pattern4();
+    pattern5();
     return 0;
 }
 
@@ -52,4 +54,31 @@ void pattern4 () {
         }
         printf("\n");
     }
+}
+
+void pattern5 () {
+    int n = 5;
+    /* One way is this which is for beginners easy to understand
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j <= i; j++){
+            printf("*");
+        }
+        printf("\n");
+    }
+    for(int i = 1; i < n; i++){
+        for(int j = n - i; j >= 1; j--){
+            printf("*");
+        }
+        printf("\n");
+    }
+    */
+   // 2nd way which is optimized as there is only one outer loop and one inner loop.
+   int rowN = (2 * n) - 1;
+   for(int i = 1; i <= rowN; i++){
+    int colN = i <= n ? i : rowN - (i-1);
+        for(int j = 0; j < colN; j++){
+            printf("*");
+        } 
+        printf("\n");
+   }
 }
